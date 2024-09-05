fb8b3c02e73069a98a4365dc6d7f4f73bdc497ca
matko-flabbergast <matko.papic@flabbergast.agency>
2024-09-05 12:13:29 +0200

    Deleted files so i can merge dev with PR

--- app/src/main/java/com/example/reduxtestapp/data/model/TodoItem.kt
+++
@@ -1,9 +0,0 @@
-package com.example.reduxtestapp.data.model
-
-import kotlinx.serialization.Serializable
-
-@Serializable
-data class TodoItem (
-    val text: String,
-    val isCompleted: Boolean
-)
